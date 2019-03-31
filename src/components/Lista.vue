93bec5d354fb6be309170e4537908352172aa77d
Romes <rstuma@gmail.com>
2019-03-31 16:22:06 -0400

    aaa

--- src/components/Lista.vue
+++ src/components/Lista.vue
@@ -1,3 +1,46 @@
 <template>
-  <h1>Lsita de Clientes</h1>
+  <v-data-table :headers="headers" :items="clientes" class="elevation-1">
+    <template v-slot:items="props">
+      <td>{{ props.item.id }}</td>
+      <td>{{ props.item.nome }}</td>
+      <td>{{ props.item.email }}</td>
+      <td>{{ props.item.fone }}</td>
+      <td>{{ props.item.conta_display }}</td>
+    </template>
+  </v-data-table>
 </template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "ID",
+          align: "left",
+          sortable: false,
+          value: "id"
+        },
+        { text: "Nome", value: "nome" },
+        { text: "E-mail", value: "email" },
+        { text: "Fone", value: "fone" },
+        { text: "Conta", value: "conta_display" }
+      ]
+    };
+  },
+  computed: {
+    ...mapState(["clientes"])
+  },
+  methods: {
+    ...mapActions(["startClientes"]),
+    reload() {
+      this.startClientes();
+    }
+  },
+  mounted() {
+    this.reload();
+  }
+};
+</script>
--- src/views/Cliente.vue
+++ src/views/Cliente.vue
@@ -1,49 +1,5 @@
 <template>
   <div id="cliente">
     <hfz-lista></hfz-lista>
-    <v-data-table :headers="headers" :items="clientes" class="elevation-1">
-      <template v-slot:items="props">
-        <td>{{ props.item.id }}</td>
-        <td>{{ props.item.nome }}</td>
-        <td>{{ props.item.email }}</td>
-        <td>{{ props.item.fone }}</td>
-        <td>{{ props.item.conta_display }}</td>
-      </template>
-    </v-data-table>
   </div>
 </template>
-
-<script>
-import { mapActions, mapState } from "vuex";
-
-export default {
-  data() {
-    return {
-      headers: [
-        {
-          text: "ID",
-          align: "left",
-          sortable: false,
-          value: "id"
-        },
-        { text: "Nome", value: "nome" },
-        { text: "E-mail", value: "email" },
-        { text: "Fone", value: "fone" },
-        { text: "Conta", value: "conta_display" }
-      ]
-    };
-  },
-  computed: {
-    ...mapState(["clientes"])
-  },
-  methods: {
-    ...mapActions(["startClientes"]),
-    reload() {
-      this.startClientes();
-    }
-  },
-  mounted() {
-    this.reload();
-  }
-};
-</script>
